e43073171759cfa5ae8e72b038567d934b237ad7
NandeBija <nandebija@gmail.com>
2022-02-14 11:59:47 +0200

    added start script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.set("port", process.env.port || 3000);
+app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res, next) => {
   res.send({ msg: "Nande Bija's portfolio" });
@@ -17,6 +17,6 @@ app.use("/projects", projectsRouter); //use the project router API//
 app.use("/testimonials", testimonialRouter); //use the testimonials router API//
 app.use("/contact", contactRouter); //use the contact router API//
 
-app.listen(app.get("port"), (server) => {
+app.listen(app.get("port"), () => {
   console.info(`Server listen on port ${app.get("port")}`);
 });
